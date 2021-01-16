4d256604f2553ecc88d1aa12bc04dd2c76319582
Oscar Baelde <oscar.baelde@student.ehb.be>
2021-01-16 10:01:54 +0100

    Fix profile

--- src/Auth/Profile.js
+++ src/Auth/Profile.js
@@ -8,11 +8,16 @@ function Profile() {
   if (isAuthenticated) {
     return (
       <>
-      <div className="left"
-        <img src={user.picture} classname="rounded float-start" alt="profile" />
-        <h2>{user.name}</h2>
-        <h2>{user.given_name}</h2>
-        <h2>{user.email_verified}</h2>
+        <div className="left">
+          <img
+            src={user.picture}
+            classname="rounded float-start"
+            alt="profile"
+          />
+          <h2>{user.name}</h2>
+          <h2>{user.given_name}</h2>
+          <h2>{user.email_verified}</h2>
+        </div>
       </>
     );
   } else return <div>Not signed in!</div>;
